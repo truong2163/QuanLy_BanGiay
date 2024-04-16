411497b5e7bb8b4c26f18d28772a968cbda9388f
truong2163 <167168499+truong2163@users.noreply.github.com>
2024-04-16 17:10:06 +0700

    Add files via upload

---
+++ FormLogin/FormLogin/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace FormLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTenDangNhap = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.buttonThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 77);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên đăng nhập:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 193);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mật khẩu:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtTenDangNhap
+            // 
+            this.txtTenDangNhap.Location = new System.Drawing.Point(312, 74);
+            this.txtTenDangNhap.Name = "txtTenDangNhap";
+            this.txtTenDangNhap.Size = new System.Drawing.Size(335, 37);
+            this.txtTenDangNhap.TabIndex = 1;
+            this.txtTenDangNhap.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Location = new System.Drawing.Point(312, 190);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(335, 37);
+            this.txtMatKhau.TabIndex = 2;
+            this.txtMatKhau.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(312, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Đăng Nhập";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Purple;
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(489, 251);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 50);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Đăng Ký";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // buttonThoat
+            // 
+            this.buttonThoat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.buttonThoat.Location = new System.Drawing.Point(164, 257);
+            this.buttonThoat.Name = "buttonThoat";
+            this.buttonThoat.Size = new System.Drawing.Size(104, 39);
+            this.buttonThoat.TabIndex = 7;
+            this.buttonThoat.Text = "Thoát";
+            this.buttonThoat.UseVisualStyleBackColor = false;
+            this.buttonThoat.Click += new System.EventHandler(this.buttonThoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(729, 449);
+            this.Controls.Add(this.buttonThoat);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtTenDangNhap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTenDangNhap;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button buttonThoat;
+    }
+}
+
---
+++ FormLogin/FormLogin/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace FormLogin
+{
+    public partial class Form1 : Form
+    {
+        // Danh sách nhân viên và mật khẩu
+        Dictionary<string, string> nhanVien = new Dictionary<string, string>()
+        {
+    { "nhanvien1", "matkhau1" },
+    { "nhanvien2", "matkhau2" },
+    { "nhanvien3", "matkhau3" },
+        };
+
+        int soLanSai = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            txtMatKhau.PasswordChar = '*';
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Regex.IsMatch(txtTenDangNhap.Text, "^[a-zA-Z0-9]+$"))
+            {
+                MessageBox.Show("Tên đăng nhập không được chứa ký tự đặc biệt.");
+                return;
+            }
+
+            // Check if the password is at least 6 characters long
+            if (txtMatKhau.Text.Length < 6)
+            {
+                MessageBox.Show("Mật khẩu phải từ 6 ký tự trở lên.");
+                return;
+            }
+            SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-RIFIUM18;Initial Catalog=QuanLy;Integrated Security=True;");
+            try
+            {
+                con.Open();
+                string tenDangNhap = txtTenDangNhap.Text;
+                string matKhau = txtMatKhau.Text;
+                string sql = "SELECT * FROM NguoiDung WHERE TaiKhoan = @TenDangNhap AND MatKhau = @MatKhau";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@TenDangNhap", tenDangNhap);
+                cmd.Parameters.AddWithValue("@MatKhau", matKhau);
+                SqlDataReader dta = cmd.ExecuteReader();
+                if (dta.Read())
+                {
+                    MessageBox.Show("Bạn đã đăng nhập thành công!");
+                }
+                else if (string.IsNullOrEmpty(tenDangNhap) || string.IsNullOrEmpty(matKhau))
+                {
+                    MessageBox.Show("Vui lòng nhập tên đăng nhập và mật khẩu!");
+                }
+                // Kiểm tra tên đăng nhập và mật khẩu
+                else if (nhanVien.ContainsKey(tenDangNhap) && nhanVien[tenDangNhap] == matKhau)
+                {
+                    soLanSai = 0;
+                    MessageBox.Show("Bạn đã đăng nhập thành công!");
+                }
+                else
+                {
+                    soLanSai++;
+                    MessageBox.Show("Tên đăng nhập hoặc mật khẩu không chính xác!");
+
+                    if (soLanSai >= 3)
+                    {
+                        MessageBox.Show("Bạn đã nhập sai quá 3 lần. Hệ thống sẽ tự động thoát!");
+                        Application.Exit();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối");
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtTenDangNhap.Text = "";
+            txtMatKhau.Text = "";
+            txtTenDangNhap.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có thực sự muốn thoát?", "Thoát", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+        private void txtMatKhau_TextChanged(object sender, EventArgs e)
+        {
+            txtMatKhau.PasswordChar = '*';
+        }
+
+        private void buttonThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có thực sự muốn thoát?", "Thoát", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
