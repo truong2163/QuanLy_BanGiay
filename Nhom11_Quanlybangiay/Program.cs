760f16d764ec77e328c39ab6ddb189162f487fa0
truong2163 <167168499+truong2163@users.noreply.github.com>
2024-05-06 14:58:58 +0700

    Add files via upload

---
+++ Nhom11_Quanlybangiay/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+using Nhom11_Quanlybangiay.FormChucNang;
+using Nhom11_Quanlybangiay.Khachhang;
+using Nhom11_Quanlybangiay.DanhSachCacNhaCungCap;
+
+using Nhom11_Quanlybangiay.NhapHang;
+using Nhom11_Quanlybangiay.SanPham;
+using Nhom11_Quanlybangiay.ThemSanPham;
+
+namespace Nhom11_Quanlybangiay.FrmLogin
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmLogin());
+        }
+    }
+}
