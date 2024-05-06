e331f7fd6cb813a9e3153a69d31f44269d7c5358
truong2163 <167168499+truong2163@users.noreply.github.com>
2024-05-06 14:56:50 +0700

    Add files via upload

---
+++ Nhom11_Quanlybangiay/bin/Debug/FrmLogin1/FrmLogin.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace Nhom11_Quanlybangiay.FrmLogin
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.txtTaiKhoan = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.ForeColor = System.Drawing.Color.Black;
+            this.checkBox1.Location = new System.Drawing.Point(105, 407);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(198, 27);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Hiển thị mật khẩu";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.Click += new System.EventHandler(this.checkBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(30, 500);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(330, 54);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Thoát";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnDangNhap.Location = new System.Drawing.Point(30, 442);
+            this.btnDangNhap.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(330, 50);
+            this.btnDangNhap.TabIndex = 3;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMatKhau.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMatKhau.Location = new System.Drawing.Point(157, 361);
+            this.txtMatKhau.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.PasswordChar = '*';
+            this.txtMatKhau.Size = new System.Drawing.Size(203, 25);
+            this.txtMatKhau.TabIndex = 1;
+            // 
+            // txtTaiKhoan
+            // 
+            this.txtTaiKhoan.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTaiKhoan.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTaiKhoan.Location = new System.Drawing.Point(157, 311);
+            this.txtTaiKhoan.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTaiKhoan.Name = "txtTaiKhoan";
+            this.txtTaiKhoan.Size = new System.Drawing.Size(203, 25);
+            this.txtTaiKhoan.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(26, 361);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(26, 311);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tài khoản";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Century Schoolbook", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(137, 119);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 54);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Xin chào";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Century", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(228, 573);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 21);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Quên mật khẩu ?";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.BackgroundImage = global::Nhom11_Quanlybangiay.Properties.Resources.pexels_hngstrm_1939485;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(389, 612);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.txtTaiKhoan);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.Load += new System.EventHandler(this.FrmLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.TextBox txtTaiKhoan;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Nhom11_Quanlybangiay/bin/Debug/FrmLogin1/FrmLogin.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using Nhom11_Quanlybangiay.FormChucNang;
+using Nhom11_Quanlybangiay.FrmLogin1;
+
+namespace Nhom11_Quanlybangiay.FrmLogin
+{
+    public partial class FrmLogin : Form
+    {
+        public string MaNQL { get; set; }
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string tk = txtTaiKhoan.Text;
+            string mk = txtMatKhau.Text;  // KIỂM TRA TÀI KHOẢN MẬT KHẨU
+            string loaitk;
+
+            SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-RIFIUM18;Initial Catalog=qlgiay;Integrated Security=True");
+            con.Open();
+            string Layratinhtrangtaikhoan = "exec layratinhtrangNQL '" + tk + "'";
+            DataTable datatb = new DataTable();
+            SqlDataAdapter adap = new SqlDataAdapter(Layratinhtrangtaikhoan, con);  // LẤY RA TÌNH TRẠNG CỦA TÀI KHOẢN
+            adap.Fill(datatb);
+             // TÌNH TRẠNG CỦA TÀI KHOẢN LÀ ĐANG HOẠT ĐỘNG HAY ĐANG KHÔNG HOẠT ĐỘNG
+            con.Close();
+
+
+
+            con.Open();
+            string sql = "Select * from Nguoiquanli where MaNQL='" + tk + "' and mat_khau='" + mk + "'   ";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlDataReader dt = cmd.ExecuteReader();
+            //NẾU ĐỌC ĐƯỢC DỮ LIỆU THÌ CÓ NGHĨA LÀ TỒN TẠI
+
+            MaNQL = tk;
+
+            if (dt.Read() == true)
+            {
+                loaitk = datatb.Rows[0][0].ToString();
+                if (loaitk.Equals("NV"))
+                {
+                    this.Hide();
+                    frmNV f = new frmNV(tk, mk);
+                    MessageBox.Show("Đăng nhập thành công với tư cách nhân viên!!!");
+                    // TRUYỀN TK MK SANG FORM KHÁC ĐỂ BÁN HÀNG XUẤT HÀNG
+                    f.ShowDialog();
+                   
+                    this.Close();
+                    
+                }
+                else if(loaitk.Equals("ADMIN"))
+                {
+                    this.Hide();
+                    frmChuCuaHang f = new frmChuCuaHang(tk, mk);
+                    MessageBox.Show("Đăng nhập thành công với tư cách chủ cửa hàng!!!");
+                    // TRUYỀN TK MK SANG FORM KHÁC ĐỂ BÁN HÀNG XUẤT HÀNG
+                    f.ShowDialog();
+                    
+                    this.Close();
+                }
+                else  if (loaitk.Equals("VHH"))
+                {
+                    MessageBox.Show("Tài khoản này bị vô hiệu hóa!!!");
+                }    
+
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại");
+            }
+
+
+
+            con.Close();
+
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+           // SET BACK COLOR
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                txtMatKhau.PasswordChar = '\0';
+            }
+            else
+                txtMatKhau.PasswordChar = '*';
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            frmQuenMatKhau f = new frmQuenMatKhau();
+            f.Show();
+        }
+    }
+}
